b96eeecaa176c045acceda3af8df21f70ff71685
Wontory <tjddnjsjo@kakao.com>
2022-11-02 10:40:21 +0900

    close button error fixed

--- todo.js
+++ todo.js
@@ -18,11 +18,11 @@ function addItem() {
 function showList() {
     let list = "<ul>"
     for (let i = 0; i < itemList.length; i++)
-        list += `<li>${itemList[i]}<span class='close' id=${i}>\u00D7`
+        list += `<li>${itemList[i]}<span class='close' id=${i}><i class="fa-solid fa-xmark"></i>`
     list += "</ul>";
     document.querySelector(".todo-list").innerHTML = list;
 
-    let deleteButtons = document.querySelector(".close");
+    let deleteButtons = document.querySelectorAll(".close");
     for (let i = 0; i < deleteButtons.length; i++)
         deleteButtons[i].addEventListener("click", deleteItem);
 }
